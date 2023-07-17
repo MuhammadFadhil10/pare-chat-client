5d69a37ac943a54498f704e591c15ebd0e5b1fbe
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-18 01:05:25 +0700

    setup

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+"use client";
+import { createTheme } from "@mui/material";
+
+export const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#FDCEDF",
+    },
+    secondary: {
+      main: "#E3F4F4",
+    },
+  },
+});
