d0b33e5428e3b4707d1046616dae943f30c6a376
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-18 11:57:38 +0700

    setup socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
 "use client";
+
+import * as React from "react";
 import { socket } from "@/utils";
-import { useTheme, Typography } from "@mui/material";
+import { useTheme } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
 export default function Home() {
+  const [msg, setMsg] = React.useState("");
+  const [history, setHistory] = React.useState<unknown[]>([]);
+
   const theme = useTheme();
 
   socket.on("connection", () => {
     console.log("connect");
   });
+
+  socket.on("message", (val) => {
+    setHistory([...history, val]);
+  });
+
+  const onSendMessage = React.useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+
+      socket.emit(
+        "message",
+        JSON.stringify({ message: msg }),
+        (response: unknown) => {
+          setMsg("");
+
+          // setHistory([...history, response]);
+        }
+      );
+    },
+    [msg]
+  );
 
   return (
     <main>
@@ -20,6 +49,14 @@ export default function Home() {
         >
           asd
         </Typography>
+
+        {history.map((text, index) => (
+          <Typography key={index}>{text as string}</Typography>
+        ))}
+
+        <form onSubmit={onSendMessage}>
+          <TextField value={msg} onChange={(e) => setMsg(e.target.value)} />
+        </form>
       </div>
     </main>
   );
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = `${process.env.API_BASE_URL}`;
+const URL = `${process.env.API_BASE_URL ?? "http://localhost:8001"}`;
 
 export const socket = io(URL, {
-  autoConnect: false,
+  autoConnect: true,
 });
