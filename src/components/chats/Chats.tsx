a7d93a9480d6c7cb755dc88e195a6bc310b51749
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-22 22:26:42 +0700

    2 user message

--- src/components/chats/Chats.tsx
+++ src/components/chats/Chats.tsx
@@ -7,8 +7,15 @@ import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { EmptyChat } from "../empty";
+import { socket } from "@/utils";
 
 const Component = () => {
+  const [history, setHistory] = React.useState<unknown[]>([]);
+
+  socket.on("message", (val) => {
+    setHistory([...history, JSON.parse(val)]);
+  });
+
   return (
     <Container maxWidth="sm" sx={{ boxShadow: 2, py: 2, height: "100vh" }}>
       <Stack gap={2}>
@@ -32,7 +39,17 @@ const Component = () => {
         {/* chat list */}
         <Typography sx={{ color: "text.primary" }}>Your chats</Typography>
 
-        <EmptyChat />
+        {history.length === 0 && <EmptyChat />}
+
+        {/* {history.map((h: any, index) => (
+          <Typography key={index}>{h.message}</Typography>
+        ))} */}
+
+        <Typography>
+          {(history[history.length - 1] as any)?.receiverId ===
+            JSON.parse(localStorage.user).id &&
+            (history[history.length - 1] as any)?.message}
+        </Typography>
       </Stack>
     </Container>
   );
--- src/components/chats/RoomChat.tsx
+++ src/components/chats/RoomChat.tsx
@@ -2,22 +2,80 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import SendIcon from "@mui/icons-material/Send";
 import { User } from "@/types";
 import { ProfileDisplay } from "../ProfileDisplay";
+import { socket } from "@/utils";
+import { ChatFriendContext } from "@/context";
 
 interface Props {
   user: User;
 }
 
 const Component = ({ user }: Props) => {
+  const [message, setMessage] = React.useState("");
+  const [history, setHistory] = React.useState<unknown[]>([]);
+
+  const { chatFriend } = React.useContext(ChatFriendContext);
+
+  React.useEffect(() => {
+    socket.on("message", (val) => {
+      const data = JSON.parse(val);
+
+      if (data.receiverId === JSON.parse(localStorage.user).id) {
+        setHistory([...history, data]);
+      }
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, [history, user.id]);
+
+  const onSendMessage = React.useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+
+      socket.emit(
+        "message",
+        JSON.stringify({
+          senderId: JSON.parse(localStorage.user).id,
+          receiverId: chatFriend?.id,
+          message,
+        }),
+        (response: string) => {
+          setMessage("");
+
+          setHistory([...history, JSON.parse(response)]);
+        }
+      );
+    },
+    [chatFriend?.id, history, message]
+  );
+
   return (
-    <Stack sx={{ width: "100vw" }}>
+    <Stack alignItems="center" sx={{ width: "100vw" }}>
       <Box sx={{ boxShadow: 1, p: 1 }}>
         <ProfileDisplay user={user} onClick={() => undefined} isStatic />
       </Box>
 
       {/* bubble container */}
-      <Stack sx={{ width: "100%", height: "80%" }}></Stack>
+      <Stack sx={{ width: "700px", height: "80%", px: 2 }}>
+        {history.map((h: any, index) => (
+          <Typography
+            key={index}
+            sx={{
+              alignSelf:
+                h.senderId === JSON.parse(localStorage.user).id
+                  ? "flex-end"
+                  : "inherit",
+            }}
+          >
+            {h.message}
+          </Typography>
+        ))}
+      </Stack>
 
       <Box
         sx={{
@@ -27,9 +85,23 @@ const Component = ({ user }: Props) => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          gap: 2,
         }}
       >
-        <TextField size="medium" sx={{ width: "70%" }} multiline minRows={1} placeholder="Message" />
+        <TextField
+          size="medium"
+          value={message}
+          sx={{ width: "70%" }}
+          multiline
+          minRows={1}
+          placeholder="Message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <SendIcon
+          fontSize="large"
+          sx={{ color: "text.primary", cursor: "pointer" }}
+          onClick={onSendMessage}
+        />
       </Box>
     </Stack>
   );
