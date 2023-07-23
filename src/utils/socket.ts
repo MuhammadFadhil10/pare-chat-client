be69096f0a4ba4d71110b82a8bd3b7e56fef0404
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-24 00:04:36 +0700

    fix: socket config

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = `${process.env.API_BASE_URL ?? "http://localhost:8001"}`;
+const URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}`;
 
 export const socket = io(URL, {
   autoConnect: true,
